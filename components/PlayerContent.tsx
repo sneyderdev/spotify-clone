3bb843fd22b1da3836369ec3e88f797588f167a9
Sneyder Barreto <hello@sneyder.dev>
2023-10-04 22:36:10 -0500

    feat: add player functionality

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,5 +1,7 @@
+// @ts-nocheck
 'use client';
 
+import { useEffect, useState } from 'react';
 import {
   IconPlayerPauseFilled,
   IconPlayerPlayFilled,
@@ -8,7 +10,9 @@ import {
   IconVolume3,
   IconVolume2,
 } from '@tabler/icons-react';
+import useSound from 'use-sound';
 
+import usePlayer from '~/hooks/usePlayer';
 import { Song } from '~/types';
 
 import LibraryItem from './LibraryItem';
@@ -21,8 +25,77 @@ interface PlayerContentProps {
 }
 
 const PlayerContent = ({ song, songUrl }: PlayerContentProps) => {
-  const Icon = true ? IconPlayerPauseFilled : IconPlayerPlayFilled;
-  const VolumeIcon = true ? IconVolume3 : IconVolume2;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const Icon = isPlaying ? IconPlayerPauseFilled : IconPlayerPlayFilled;
+  const VolumeIcon = volume === 0 ? IconVolume3 : IconVolume2;
+
+  const [play, { pause, sound }] = useSound(songUrl, {
+    volume,
+    onplay: () => setIsPlaying(true),
+    onend: () => {
+      setIsPlaying(false);
+      onPlayNext();
+    },
+    onpause: () => setIsPlaying(false),
+    format: ['mp3'],
+  });
+
+  useEffect(() => {
+    sound?.play();
+
+    return () => {
+      sound?.unload();
+    };
+  }, [sound]);
+
+  const handlePlay = () => {
+    if (!isPlaying) {
+      play();
+    } else {
+      pause();
+    }
+  };
+
+  const toggleMute = () => {
+    if (volume === 0) {
+      setVolume(1);
+    } else {
+      setVolume(0);
+    }
+  };
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong);
+  };
+
+  const onPlayPrevious = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const previousSong = player.ids[currentIndex - 1];
+
+    if (!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+
+    player.setId(previousSong);
+  };
 
   return (
     <div className="grid h-full grid-cols-2 md:grid-cols-3">
@@ -34,32 +107,32 @@ const PlayerContent = ({ song, songUrl }: PlayerContentProps) => {
       </div>
       <div className="col-auto flex w-full items-center justify-end md:hidden">
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white p-1"
         >
           <Icon className="text-black" />
         </div>
       </div>
       <div className="hidden h-full w-full max-w-[722px] items-center justify-center gap-x-6 md:flex">
-        <button type="button" onClick={() => {}}>
+        <button type="button" onClick={onPlayPrevious}>
           <IconPlayerSkipBackFilled className="text-neutral-400 transition hover:text-white" />
         </button>
         <div
-          onClick={() => {}}
+          onClick={handlePlay}
           className="flex h-10 w-10 cursor-pointer items-center justify-center rounded-full bg-white p-1"
         >
           <Icon className="text-black" />
         </div>
-        <button type="button" onClick={() => {}}>
+        <button type="button" onClick={onPlayNext}>
           <IconPlayerSkipForwardFilled className="text-neutral-400 transition hover:text-white" />
         </button>
       </div>
       <div className="hidden w-full justify-end pr-2 md:flex">
         <div className="flex w-[120px] items-center gap-x-2">
-          <button type="button">
+          <button type="button" onClick={toggleMute}>
             <VolumeIcon />
           </button>
-          <Slider />
+          <Slider value={volume} onChange={(value) => setVolume(value)} />
         </div>
       </div>
     </div>
